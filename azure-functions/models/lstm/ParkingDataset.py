0896a3b5757c50f9da8123238812e3fcc3b37e59
Jacky Huang <lashhw81@gmail.com>
2022-05-30 12:06:54 +0800

    new lstm architecture

--- azure-functions/models/lstm/ParkingDataset.py
+++ azure-functions/models/lstm/ParkingDataset.py
@@ -5,57 +5,56 @@ import torch.nn.functional as F
 
 
 class ParkingDataset(Dataset):
-    def __init__(self, sequence, is_valid, weekdays, seconds, predict_len):
+    def __init__(self, sequence, is_valid, weekdays, seconds,
+                 window_len, predict_len):
         self.sequence = torch.tensor(sequence).to(torch.float)
         self.is_valid = torch.tensor(is_valid).to(torch.float)
         self.p_lot_len, self.seq_len = self.sequence.shape
 
         weekdays_onehot = F.one_hot(torch.tensor(weekdays), num_classes=7)
-        assert weekdays_onehot.shape == (self.seq_len, 7)
+        # assert weekdays_onehot.shape == (self.seq_len, 7)
 
         seconds_in_day = 24 * 60 * 60
         seconds_rad = 2 * np.pi * (seconds/seconds_in_day)
         seconds_cos = torch.tensor(np.cos(seconds_rad)).unsqueeze(1)
         seconds_sin = torch.tensor(np.sin(seconds_rad)).unsqueeze(1)
-        assert seconds_cos.shape == (self.seq_len, 1)
-        assert seconds_sin.shape == (self.seq_len, 1)
+        # assert seconds_cos.shape == (self.seq_len, 1)
+        # assert seconds_sin.shape == (self.seq_len, 1)
 
         self.timeinfo = torch.cat([weekdays_onehot, seconds_cos, seconds_sin], dim=1) \
                              .to(torch.float)
-        assert self.timeinfo.shape == (self.seq_len, 9)
+        # assert self.timeinfo.shape == (self.seq_len, 9)
 
+        self.window_len = window_len
         self.predict_len = predict_len
 
 
     def __len__(self):
-        return self.p_lot_len
+        return self.p_lot_len * (self.seq_len-self.window_len-self.predict_len+1)
 
 
     def __getitem__(self, idx):
-        part_seq_len = self.seq_len - self.predict_len
+        p_lot_idx     = idx // (self.seq_len-self.window_len-self.predict_len+1)
+        seq_start_idx = idx %  (self.seq_len-self.window_len-self.predict_len+1)
 
-        x_seq = self.sequence[idx, :-self.predict_len]
-        assert x_seq.shape == (part_seq_len,)
+        x_seq = self.sequence[p_lot_idx, seq_start_idx:
+                              seq_start_idx+self.window_len]
+        # assert x_seq.shape == (self.window_len,)
 
-        x_timeinfo = self.timeinfo[:-self.predict_len]
-        assert x_timeinfo.shape == (part_seq_len, 9)
+        x_timeinfo = self.timeinfo[seq_start_idx+self.window_len-1]
+        # assert x_timeinfo.shape == (9,)
 
-        x_p_lot = F.one_hot(torch.tensor(idx), num_classes=self.p_lot_len)
-        x_p_lot = x_p_lot.unsqueeze(0)
-        assert x_p_lot.shape == (1, self.p_lot_len)
+        x_p_lot = F.one_hot(torch.tensor(p_lot_idx), num_classes=self.p_lot_len)
+        # assert x_p_lot.shape == (self.p_lot_len,)
 
-        x_p_lot = x_p_lot.expand(part_seq_len, -1)
-        assert x_p_lot.shape == (part_seq_len, self.p_lot_len)
+        x_extra = torch.cat([x_timeinfo, x_p_lot], dim=0)
+        # assert x_extra.shape == (9+self.p_lot_len,)
 
-        x_extra = torch.cat([x_timeinfo, x_p_lot], dim=1)
-        assert x_extra.shape == (part_seq_len, 9+self.p_lot_len)
-
-        y = torch.empty(part_seq_len, self.predict_len)
-        y_valid = torch.empty(part_seq_len, self.predict_len)
-        for i in range(self.predict_len):
-            y[:, i] = self.sequence[idx, i+1:i+1+part_seq_len]
-            y_valid[:, i] = self.is_valid[idx, i+1:i+1+part_seq_len]
-        assert y.shape == (part_seq_len, self.predict_len)
-        assert y_valid.shape == (part_seq_len, self.predict_len)
+        y = self.sequence[p_lot_idx, seq_start_idx+self.window_len:
+                          seq_start_idx+self.window_len+self.predict_len]
+        y_valid = self.is_valid[p_lot_idx, seq_start_idx+self.window_len:
+                                seq_start_idx+self.window_len+self.predict_len]
+        # assert y.shape == (self.predict_len,)
+        # assert y_valid.shape == (self.predict_len,)
 
         return x_seq, x_extra, y, y_valid
--- azure-functions/models/lstm/ParkingModel.py
+++ azure-functions/models/lstm/ParkingModel.py
@@ -4,59 +4,47 @@ import torch.nn.functional as F
 
 
 class ParkingModel(nn.Module):
-    def __init__(self, p_lot_len, predict_len):
+    def __init__(self, p_lot_len, window_len, predict_len):
         super().__init__()
         
         self.p_lot_len = p_lot_len
+        self.window_len = window_len
         self.predict_len = predict_len
 
         self.rnn = nn.LSTM(1, 128, num_layers=1, batch_first=True)
+
         self.fc1 = nn.Linear(128+9+p_lot_len, 128)
         self.fc2 = nn.Linear(128, 128)
         self.fc3 = nn.Linear(128, predict_len)
 
-    def forward(self, input_seq, input_extra, h_in=None, feed_last_only=False):
-        batch_size = input_seq.shape[0]
-        seq_len = input_seq.shape[1]
+        self.bn1 = nn.BatchNorm1d(128)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(128)
+
+    def forward(self, input_seq, input_extra):
+        assert input_seq.ndim == 2 and input_seq.shape[1] == self.window_len
+
         input_seq = input_seq.unsqueeze(2)
-        assert input_seq.shape == (batch_size, seq_len, 1)
+        # (N, L, 1)
 
-        if h_in is None:
-            out, h_out = self.rnn(input_seq)
-        else:
-            out, h_out = self.rnn(input_seq, h_in)
-        assert out.shape == (batch_size, seq_len, 128)
+        x, _ = self.rnn(input_seq)
+        # (N, L, 128)
+        x = x[:, -1, :]
+        x = self.bn1(x)
+        # (N, 128)
 
-        if feed_last_only:
-            x = out[:, -1, :]
-            assert x.shape == (batch_size, 128)
+        x = torch.cat((x, input_extra), dim=1)
+        # (N, 128+9+p_lot_len)
+        
+        x = F.relu(self.fc1(x))
+        x = self.bn2(x)
+        # (N, 128)
 
-            assert input_extra.shape == (batch_size, 9+self.p_lot_len)
+        x = F.relu(self.fc2(x))
+        x = self.bn3(x)
+        # (N, 128)
 
-            x = torch.cat((x, input_extra), dim=1)
-            assert x.shape == (batch_size, 128+9+self.p_lot_len)
+        x = self.fc3(x)
+        # (N, predict_len)
 
-            x = F.relu(self.fc1(x))
-            assert x.shape == (batch_size, 128)
-
-            x = F.relu(self.fc2(x))
-            assert x.shape == (batch_size, 128)
-
-            x = self.fc3(x)
-            assert x.shape == (batch_size, self.predict_len)
-        else:
-            assert input_extra.shape == (batch_size, seq_len, 9+self.p_lot_len)
-
-            x = torch.cat((out, input_extra), dim=2)
-            assert x.shape == (batch_size, seq_len, 128+9+self.p_lot_len)
-
-            x = F.relu(self.fc1(x))
-            assert x.shape == (batch_size, seq_len, 128)
-
-            x = F.relu(self.fc2(x))
-            assert x.shape == (batch_size, seq_len, 128)
-
-            x = self.fc3(x)
-            assert x.shape == (batch_size, seq_len, self.predict_len)
-
-        return x, h_out
+        return x
