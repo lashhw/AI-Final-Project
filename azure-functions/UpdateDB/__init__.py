00ff15bef1057c4698655a4de8c171d67d744e99
Jacky Huang <lashhw81@gmail.com>
2022-06-23 12:13:19 +0800

    periodically delete parking space history

--- azure-functions/UpdateDB/__init__.py
+++ azure-functions/UpdateDB/__init__.py
@@ -2,7 +2,7 @@ import logging
 import azure.functions as func
 from azure.data.tables import TableServiceClient
 from azure.data.tables import TableEntity
-from datetime import datetime
+from datetime import datetime, timedelta
 from urllib.request import urlopen
 import pandas as pd
 import numpy as np
@@ -15,11 +15,12 @@ connection_string = config['CONNECTION']['ConnectionString']
 ids_train = np.load('./models/common/files/ids_train.npy')
 
 table_service = TableServiceClient.from_connection_string(conn_str=connection_string)
+table_client = table_service.get_table_client(table_name='parking')
 
 url = 'https://data.ntpc.gov.tw/api/datasets/E09B35A5-A738-48CC-B0F5-570B67AD9C78/csv/file'
 
 
-def update_db():
+def main(mytimer: func.TimerRequest) -> None:
 	response = urlopen(url)
 	date_str = datetime.utcnow().strftime('%Y-%m-%d')
 	time_str = datetime.utcnow().strftime('%Y-%m-%d %H:%M')
@@ -35,9 +36,12 @@ def update_db():
 			value = str(data['AVAILABLECAR'])
 			task[column] = value
 
-	table_client = table_service.get_table_client(table_name='parking')
 	table_client.create_entity(entity=task)
 
-
-def main(mytimer: func.TimerRequest) -> None:
-    update_db()
+	old_date_str = (datetime.utcnow()-timedelta(days=7)).strftime('%Y-%m-%d')
+	old_time_str = (datetime.utcnow()-timedelta(days=7)).strftime('%Y-%m-%d %H:%M')
+	old_query_string = f"PartitionKey le '{old_date_str}' and RowKey lt '{old_time_str}'"
+	old_entities = table_client.query_entities(old_query_string, 
+											   select=['PartitionKey', 'RowKey'])
+	for e in old_entities:
+		table_client.delete_entity(e['PartitionKey'], e['RowKey'])
